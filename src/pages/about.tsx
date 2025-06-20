2b9cd8e5d2a369d8867445821c7321427e842dec
inlits <inlits26@gmail.com>
2025-06-20 13:25:53 +0500

    Updated about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -437,7 +437,7 @@ export function AboutPage() {
                 delay: 0.1
               },
               {
-                name: "inlits Member",
+                name: "Inlits Member",
                 price: "$5.99",
                 period: "per month",
                 description: "For serious learners",
