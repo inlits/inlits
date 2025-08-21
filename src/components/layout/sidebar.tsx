97c8db6fb46af3d85527194a125149353ead058c
inlits <inlits26@gmail.com>
2025-08-21 15:49:23 +0500

    Fix useSearchParams import error

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
 import { useAuth } from '@/lib/auth';
 import {
   Home,
